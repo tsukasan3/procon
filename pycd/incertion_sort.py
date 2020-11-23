d9f8759dc456a87dec9b233e048224d805b0a2e2
tsukasan3 <aterm0513@gmail.com>
2020-11-23 18:54:31 +0900

    プロコン

---
+++ pycd/incertion_sort.py
@@ -0,0 +1,13 @@
+N = int(input())
+A = list(map(int, input().split()))
+
+print(" ".join(map(str, A)))
+
+for i in range(1, N):
+    v = A[i]
+    j = i - 1
+    while (j >= 0) and (A[j] > v):
+        A[j+1] = A[j]
+        j = j - 1
+    A[j+1] = v
+    print(" ".join(map(str, A)))
